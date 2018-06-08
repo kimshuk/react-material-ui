b885d8198ee1c766d68dd8e02d9626d8c7d73f88
developer <jeesoo2002@gmail.com>
2018-06-08 16:59:34 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
-import Header from "./components/Layout/Header";
-import Footer from "./components/Layout/Footer";
+import { Header, Footer } from "./components/Layout";
 
 export default class App extends Component {
   render() {
