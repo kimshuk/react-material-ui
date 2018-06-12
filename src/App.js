ce951b8e2cceb101fc6b46f2a702001a466e48ad
developer <jeesoo2002@gmail.com>
2018-06-12 15:39:19 -0700

    add list component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React, { Component, Fragment } from "react";
 import "./App.css";
 import { Header, Footer } from "./components/Layout";
 import Exercises from "./components/Exercises";
+import { muscles, exercises } from "./store";
 
 export default class App extends Component {
+  state = {
+    exercises
+  };
+
+  getExercisesByMuscles() {
+    return Object.entries(
+      this.state.exercises.reduce((exercises, exercise) => {
+        const { muscles } = exercise;
+
+        // console.log(muscles, " muscles");
+        // console.log(exercise, " exercise");
+        // console.log(exercises, " exercises");
+        // console.log(exercises[muscles], " exercises[muscles]");
+
+        exercises[muscles] = exercises[muscles]
+          ? [...exercises[muscles], exercise]
+          : [exercise];
+
+        return exercises;
+      }, {})
+    );
+  }
+
   render() {
+    console.log(this.getExercisesByMuscles());
+    const exercises = this.getExercisesByMuscles();
+
     return (
       <Fragment>
         <Header />
-        <Exercises />
+        <Exercises exercises={exercises} />
 
-        <Footer />
+        <Footer muscles={muscles} />
       </Fragment>
     );
   }
--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,19 +1,43 @@
-import React from "react";
-import { Grid, Paper } from "@material-ui/core";
-import LeftPane from "./LeftPane";
-import RightPane from "./RightPane";
+import React, { Fragment } from "react";
+import {
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core";
 
 const styles = {
   Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
 };
 
-export default props => (
+export default ({ exercises }) => (
   <Grid container>
     <Grid item sm>
-      <LeftPane styles={styles} />
+      <Paper style={styles.Paper}>
+        {exercises.map(([group, exercises]) => (
+          <Fragment>
+            <Typography
+              variant="headline"
+              style={{ textTransform: "capitalize" }}
+            >
+              {group}
+            </Typography>
+            <List component="nav">
+              <ListItem button>
+                <ListItemText primary="Trash" />
+              </ListItem>
+              <ListItem button component="a" href="#simple-list">
+                <ListItemText primary="Spam" />
+              </ListItem>
+            </List>
+          </Fragment>
+        ))}
+      </Paper>
     </Grid>
     <Grid item sm>
-      <RightPane styles={styles} />
+      <Paper style={styles.Paper}>Right Pane</Paper>
     </Grid>
   </Grid>
 );
