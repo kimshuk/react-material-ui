15c355f70e5a0845d89611a93cb4429eebfaebc8
Andrew Kim <jeesoo2002@gmail.com>
2018-06-11 20:48:08 -0700

    material ui header and footer added

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,3 +1,8 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
 
-export default props => <div>Hello from Exercises</div>;
+export default props => (
+  <Grid container>
+    <Grid item>Left Pane</Grid>
+  </Grid>
+);
