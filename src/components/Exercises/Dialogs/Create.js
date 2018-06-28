5be866eba6f1c45ee3d8c3633e0c6e9be9805361
developer <jeesoo2002@gmail.com>
2018-06-28 14:23:20 -0700

    added dialog and icon

--- src/components/Exercises/Dialogs/Create.js
+++ src/components/Exercises/Dialogs/Create.js
@@ -1,2 +1,48 @@
-import React from 'react'
+import React, { Fragment, Component } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import AddIcon from "@material-ui/icons/Add";
 
+export default class Create extends Component {
+  state = {
+    open: false
+  };
+
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <Fragment>
+        <Button variant="fab" onClick={this.handleToggle} mini>
+          <AddIcon />
+        </Button>
+        <Dialog open={open} onClose={this.handleToggle}>
+          <DialogTitle id="form-dialog-title">
+            Create a New Exercise
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please fill out the form below.
+            </DialogContentText>
+          </DialogContent>
+          <form />
+          <DialogActions>
+            <Button color="primary" variant="raised">
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Fragment>
+    );
+  }
+}
